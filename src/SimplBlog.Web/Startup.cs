dd24479b6106eac679ff71a53a1782e741c6af6b
trungct <trungcaot@hotmail.com>
2019-03-23 23:41:52 +0700

    Got a message from configuration file.

--- src/SimplBlog.Web/Startup.cs
+++ src/SimplBlog.Web/Startup.cs
@@ -6,11 +6,23 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace SimplBlog.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,7 +39,8 @@ namespace SimplBlog.Web
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Welcome SimplBlog!");
+                var message = Configuration["Message"];
+                await context.Response.WriteAsync(message);
             });
         }
     }

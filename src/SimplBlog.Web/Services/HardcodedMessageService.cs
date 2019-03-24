1ad00603835d887f4b59a5abbbd178781b5f5afb
trungct <trungcaot@hotmail.com>
2019-03-24 10:20:59 +0700

    Get message from service.

---
+++ src/SimplBlog.Web/Services/HardcodedMessageService.cs
@@ -0,0 +1,16 @@
+﻿using SimplBlog.Web.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimplBlog.Web.Services
+{
+    public class HardcodedMessageService : IMessageService
+    {
+        public string GetMessage()
+        {
+            return "Hardcode message from a service.";
+        }
+    }
+}
--- src/SimplBlog.Web/Startup.cs
+++ src/SimplBlog.Web/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using System.IO;
+using SimplBlog.Web.Services;
+using SimplBlog.Web.Services.Interfaces;
 
 namespace SimplBlog.Web
 {
@@ -27,10 +29,12 @@ namespace SimplBlog.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IMessageService, HardcodedMessageService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        // This method gets called by the runtime. 
+        // Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IMessageService msg)
         {
             if (env.IsDevelopment())
             {
@@ -39,8 +43,12 @@ namespace SimplBlog.Web
 
             app.Run(async (context) =>
             {
-                var message = Configuration["Message"];
-                await context.Response.WriteAsync(message);
+                // -- Get message from configuration file. -- //
+                //var message = Configuration["Message"];
+                //await context.Response.WriteAsync(message);
+
+                // -- Get message from service. -- //
+                await context.Response.WriteAsync(msg.GetMessage());
             });
         }
     }

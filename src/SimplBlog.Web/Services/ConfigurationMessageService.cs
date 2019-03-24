fbd34c08660df1dc0265e3451ca35d6f4c062a2a
trungct <trungcaot@hotmail.com>
2019-03-24 10:40:31 +0700

    Created ConfigurationMessageService get message from configuration file.

---
+++ src/SimplBlog.Web/Services/ConfigurationMessageService.cs
@@ -0,0 +1,24 @@
+﻿using SimplBlog.Web.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace SimplBlog.Web.Services
+{
+    public class ConfigurationMessageService : IMessageService
+    {
+        public IConfiguration _configuration { get; set; }
+
+        public ConfigurationMessageService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetMessage()
+        {
+            return _configuration["Message"];
+        }
+    }
+}
--- src/SimplBlog.Web/Startup.cs
+++ src/SimplBlog.Web/Startup.cs
@@ -29,7 +29,8 @@ namespace SimplBlog.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IMessageService, HardcodedMessageService>();
+            services.AddSingleton(provider => Configuration);
+            services.AddSingleton<IMessageService, ConfigurationMessageService>();
         }
 
         // This method gets called by the runtime. 
